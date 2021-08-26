e58e1b951d21528676b45406d0d30f4a8770ab16
jmvt717 <jmvt717@josephmilton.world>
2021-08-25 23:14:58 -0500

    update commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,76 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // {
+    //     type: "input",
+    //     message: "Title?",
+    //     name: "Title",
+    // },
+    // {
+    //     type: "input",
+    //     message: "Description?",
+    //     name: "Description",
+    // },
+    // {
+    //     type: "input",
+    //     message: "table of contents",
+    //     name:"table of contents"
+    // },
+    // {
+    //     type: "input",
+    //     message: "Installation?",
+    //     name:"installation"
+    // },
+    // {
+    //     type: "input",
+    //     message: "Usage?",
+    //     name:"usage"
+    // },
+    {
+        type: "list",
+        message: "License?",
+        choices: ("yes", "no"),
+        name:"license"
+    },
+    // {
+    //     type: "input",
+    //     message: "Contributing?",
+    //     name:"contributing"
+    // },
+    // {
+    //     type: "input",
+    //     message: "tests?",
+    //     name:"tests"
+    // },
+    // {
+    //     type: "input",
+    //     message: "questions?",
+    //     name:"Questions"
+    // }
+];
+
+inquirer.prompt(questions)
+.then((response) =>{
+  console.log(response)
+
+    fs.writeFile("README.md", generateMarkdown (response), (err) =>
+    err ? console.log(err) : console.log("Success!"))
+});
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// const generateMarkdown = (response) => {
+//     console.log(response)
+
+//     fs.writeFile("README.md", generateMarkdown (response), (err) =>
+//     err ? console.log
+    
+// }
+
+
 
 // TODO: Create a function to initialize app
 function init() {}
